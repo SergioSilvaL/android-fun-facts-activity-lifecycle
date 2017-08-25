5e02c3d097ad887429cddbc638ac8172bb41fcec
sergiosilva <sergio_silva@tecnologiasintech.com>
2017-08-25 15:44:08 -0700

    Retrieving Instance State

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -1,6 +1,7 @@
 package com.teamtreehouse.funfacts;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,15 +18,27 @@ public class FunFactsActivity extends Activity {
     private RelativeLayout mRelativeLayout;
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
-    private String mFact;
-    private int mColor;
+    private String mFact = mFactBook.mFacts[0];
+    private int mColor = Color.parseColor(mColorWheel.mColors[8]);
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
         outState.putString(KEY_FACT, mFact);
+
         outState.putInt(KEY_COLOR, mColor);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        mFact = savedInstanceState.getString(KEY_FACT);
+        mFactLabel.setText(mFact);
+        mColor = savedInstanceState.getInt(KEY_COLOR);
+        mRelativeLayout.setBackgroundColor(mColor);
+        mShowFactButton.setTextColor(mColor);
     }
 
     @Override
